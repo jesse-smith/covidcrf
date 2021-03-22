d04487d3a10ead257ca3f9ac29a82b6365cd5a1b
Jesse Smith <jesse.smith@shelbycountytn.gov>
2021-03-22 10:11:20 -0500

    Add separate archive functionality; finish testing

--- R/manage_crf_archive.R
+++ R/manage_crf_archive.R
@@ -12,12 +12,57 @@
 #'
 #' @export
 manage_crf_archive <- function(
-  dir = "V:/EPI DATA ANALYTICS TEAM/CRF/CRF_NBS",
+  dir = paste0(
+    "V:/OPERATIONS Surveillance Coordinator/Clusters/Case Report Forms/CRF_NBS"
+  ),
   min = 7L
 ) {
+
+  regex <- ".*/CRF_NBS[^/]*[.]xlsx$"
+
+  old_files <- old_crf_files(dir = dir, regex = regex)
+
+  archive_paths <- coviData::path_create(
+    fs::path_dir(old_files),
+    "archive",
+    fs::path_file(old_files)
+  )
+
+  fs::file_move(old_files, new_path = archive_paths)
+
   coviData::trim_backups(
-    directory = coviData::path_create(dir),
-    pattern = ".*/CRF_NBS(_[0-9]{1-4}-[0-9]{1-2}-[0-9]{1-4})?[.]xlsx$",
+    directory = coviData::path_create(dir, "archive"),
+    pattern = regex,
     min_backups = min
   )
+}
+
+#' Identify Old CRF Files
+#'
+#' `old_crf_files()` identifies all CRF files in a directory except the most
+#' recent
+#'
+#' @param dir Directory to search
+#'
+#' @param regex Regular expression matching CRF files
+#'
+#' @return An `fs_path` vector
+#'
+#' @keywords internal
+old_crf_files <- function(
+  dir = paste0(
+    "V:/OPERATIONS Surveillance Coordinator/Clusters/Case Report Forms/CRF_NBS"
+  ),
+  regex = ".*/CRF_NBS[^/]*[.]xlsx$"
+) {
+  fs::dir_info(
+    coviData::path_create(dir),
+    type = "file",
+    regexp = regex
+  ) %>%
+    dplyr::filter(
+      .data[["birth_time"]] != max(.data[["birth_time"]], na.rm = TRUE)
+    ) %>%
+    dplyr::pull("path") %>%
+    vctrs::vec_sort()
 }
